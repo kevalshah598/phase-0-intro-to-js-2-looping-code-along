f62aed8652beb927ac291f9dfef79eb46d62b0fd
Keval Shah <kevalshah598@gmail.com>
2023-06-29 12:50:57 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, adjective){
+    let finalArray = []
+    for (let i = 0; i < array.length; i += 1){
+        finalArray.push("Thank you, " + array[i] + ", for the wonderful " + adjective + " gift!")
+    }
+    return finalArray
+}
+
+function countDown(num){
+    for (let i = num; i >= 0; i -= 1){
+        console.log(i)
+    }
+}
